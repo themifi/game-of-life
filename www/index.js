04c1bdaed69186dc9970a66b6967fa3e3003950e
Michael Fisher <mifi@hey.com>
2023-01-21 21:58:23 +0300

    FPS counter

--- www/index.js
+++ www/index.js
@@ -43,8 +43,53 @@ playPauseButton.addEventListener("click", event => {
         pause();
     }
 });
-  
+
+const fps = new class {
+    constructor() {
+        this.fps = document.getElementById("fps");
+        this.frames = [];
+        this.lastFrameTimeStamp = performance.now();
+    }
+
+    render() {
+        // Convert the delta time since the last frame render into a measure
+        // of frames per second.
+        const now = performance.now();
+        const delta = now - this.lastFrameTimeStamp;
+        this.lastFrameTimeStamp = now;
+        const fps = 1 / delta * 1000;
+
+        // Save only the latest 100 timings.
+        this.frames.push(fps);
+        if (this.frames.length > 100) {
+            this.frames.shift();
+        }
+
+        // Find the max, min, and mean of our 100 latest timings.
+        let min = Infinity;
+        let max = -Infinity;
+        let sum = 0;
+        for (let i = 0; i < this.frames.length; i++) {
+            sum += this.frames[i];
+            min = Math.min(this.frames[i], min);
+            max = Math.max(this.frames[i], max);
+        }
+        let mean = sum / this.frames.length;
+
+        // Render the statistics.
+        this.fps.textContent = `
+Frames per Second:
+         latest = ${Math.round(fps)}
+avg of last 100 = ${Math.round(mean)}
+min of last 100 = ${Math.round(min)}
+max of last 100 = ${Math.round(max)}
+`.trim();
+    }
+};
+
 const renderLoop = () => {
+    fps.render();
+
     universe.tick();
 
     drawGrid();
@@ -106,22 +151,22 @@ const drawCells = () => {
 
 canvas.addEventListener("click", event => {
     const boundingRect = canvas.getBoundingClientRect();
-  
+
     const scaleX = canvas.width / boundingRect.width;
     const scaleY = canvas.height / boundingRect.height;
-  
+
     const canvasLeft = (event.clientX - boundingRect.left) * scaleX;
     const canvasTop = (event.clientY - boundingRect.top) * scaleY;
-  
+
     const row = Math.min(Math.floor(canvasTop / (CELL_SIZE + 1)), height - 1);
     const col = Math.min(Math.floor(canvasLeft / (CELL_SIZE + 1)), width - 1);
-  
+
     universe.toggle_cell(row, col);
-  
+
     drawGrid();
     drawCells();
 });
-  
+
 drawGrid();
 drawCells();
 play();
